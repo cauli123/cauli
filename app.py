f8ce95a9edf906920b576c0b4168cff38dfa1040
cauli123 <44478345+cauli123@users.noreply.github.com>
2018-10-28 12:34:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    print('signature:'+str(signature)+'body'+str(body)+'app.logger.info'+str(app.logger.info))
+    print('signature:'+str(signature)+'/n'+'body'+str(body)+'/n'+'app.logger.info'+str(app.logger.info))
     # handle webhook body
     try:
         handler.handle(body, signature)
@@ -28,7 +28,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print(event.message.text)
+    print('message='event.message.text)
     if event.message.text==u"==":
         line_bot_api.reply_message(event.reply_token,
         TextSendMessage(u"都2018還有人==不加空格"))
